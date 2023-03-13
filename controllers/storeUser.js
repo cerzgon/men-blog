3380ca57949a7e83f6d3b9bb39f835cdc98129a3
cerzgon <87278525+cerzgon@users.noreply.github.com>
2023-03-13 13:25:02 -0230

    validation errors

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,7 +4,9 @@ const path = require('path')
 module.exports = (req,res) =>{
     User.create(req.body, (error, user) =>{
         if(error){
-           return res.redirect('/auth/register') 
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+            req.session.validationErrors = validationErrors
+            return res.redirect('/auth/register') 
         }
         res.redirect('/')
     })
